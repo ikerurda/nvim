8164c788599d647bc82aa4ac15e2c4e166300844
ikerurda <ikerurda@gmail.com>
2021-10-18 22:36:00 +0200

    improvements

--- install.sh
+++ install.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
+y=0
+v=0
+p=0
+n=0
 
-if [[ $1 = "-y" ]]; then
-	dir=$HOME/.config/nvim/
-	mkdir -vp ~/.config/nvim 2> /dev/null
-	cp -v init.lua $dir
-	cp -vR lua $dir
-else
-	echo "You are about to install a nvim config,"
-	read -p "[!] do you wish to continue? (y/n [n]) " cont
-	if [[ $cont != "y" ]]; then
-		exit
-	fi
-	dir=$HOME/.config/nvim/
-	mkdir -vp ~/.config/nvim 2> /dev/null
-	cp -vi init.lua $dir
-	cp -vRi lua $dir
+print_usage() {
+  printf "usage: install [-yvp]\n\n  y: skip confirmation\n  v: verbose\n  p: install plugins\n"
+}
+
+while getopts 'yvp' flag; do
+  case $flag in
+    y) y=1 ;;
+    v) v=1 ;;
+    p) p=1 ;;
+    *) n=1 ;;
+  esac
+done
+
+if [[ y && p ]]; then
+	print_usage
+	exit 1
 fi
 
-echo "Installing plugins... please wait"
+dir=$HOME/.config/nvim/
+if [[ !p ]]; then
+	if [[ n ]]; then
+		printf "You are about to install a nvim config,"
+		read -p "[!] do you wish to continue? (y/n [n]) " cont
+		if [[ $cont != "y" ]]; then
+			exit 1
+		fi
+	fi
+	((v)) && mkdir -vp ~/.config/nvim 2> /dev/null
+	((!v)) && mkdir -p ~/.config/nvim 2> /dev/null
+	((v)) && cp -v init.lua $dir
+	((!v)) && cp init.lua $dir
+	((v)) && cp -vR lua $dir
+	((!v)) && cp -R lua $dir
+fi
+
+printf "Installing plugins... please wait"
 nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
