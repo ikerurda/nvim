5cf16047a4bbbb684a24b429375f7215c1f0128e
ikerurda <ikerurda@gmail.com>
2021-11-10 10:28:11 +0100

    netrw and zip are back

--- lua/iker/disable_builtin.lua
+++ lua/iker/disable_builtin.lua
@@ -1,8 +1,8 @@
 local g = vim.g
 
 g.loaded_gzip = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
+-- g.loaded_zip = 1
+-- g.loaded_zipPlugin = 1
 g.loaded_tar = 1
 g.loaded_tarPlugin = 1
 
@@ -17,6 +17,6 @@ g.loaded_matchit = 1
 g.loaded_logiPat = 1
 g.loaded_rrhelper = 1
 
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
-g.loaded_netrwSettings = 1
+-- g.loaded_netrw = 1
+-- g.loaded_netrwPlugin = 1
+-- g.loaded_netrwSettings = 1
