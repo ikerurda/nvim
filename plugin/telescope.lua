425e445190e181809579a0e568211b4a13214afe
ikerurda <ikerurda@gmail.com>
2021-11-21 00:21:18 +0100

    temporal fix for telescope browser mappings

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,6 +1,9 @@
 local has_tl, tl = pcall(require, "telescope")
 if not has_tl then return end
 
+-- This will be removed when https://github.com/nvim-telescope/telescope-file-browser.nvim/pull/6 is fixed
+local function fb_action(f) return function(b) require"telescope".extensions.file_browser.actions[f](b) end end
+
 require"project_nvim".setup {
 	update_cwd = true,
 	update_focused_file = {enable = true, update_cwd = true, ignore_lsp = {"efm"}}
@@ -14,11 +17,6 @@ tl.setup {
 		current_buffer_fuzzy_find = {theme = "dropdown"},
 		help_tags = {theme = "dropdown"},
 		live_grep = {theme = "dropdown"},
-		file_browser = {
-			theme = "ivy",
-			layout_config = {height = .5},
-			mappings = {i = {["<C-v>"] = "move_file", ["<C-x>"] = "remove_file", ["<C-d>"] = "preview_scrolling_down"}}
-		},
 		buffers = {
 			theme = "dropdown",
 			previewer = false,
@@ -27,11 +25,17 @@ tl.setup {
 	},
 	extensions = {
 		fzf = {fuzzy = true, override_generic_sorter = true, override_file_sorter = true, case_mode = "smart_case"},
+		file_browser = {
+			theme = "ivy",
+			layout_config = {height = .5},
+			mappings = {
+				i = {["<C-v>"] = fb_action "move_file", ["<C-x>"] = fb_action "remove_file", ["<C-d>"] = "preview_scrolling_down"}
+			}
+		},
 		packer = {theme = "ivy", layout_config = {height = .5}},
-		["ui-select"] = {require("telescope.themes").get_cursor {}}
 	}
 }
 tl.load_extension "fzf"
-tl.load_extension "ui-select"
+tl.load_extension "file_browser"
 tl.load_extension "projects"
 tl.load_extension "packer"
