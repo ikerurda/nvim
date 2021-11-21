0ad5128fbdfbdd10965b8ed8edbd16e63b9108d7
ikerurda <ikerurda@gmail.com>
2021-11-22 00:16:42 +0100

    packer now opens in a buffer

--- lua/iker/plugins.lua
+++ lua/iker/plugins.lua
@@ -4,7 +4,6 @@ vim.cmd "packadd packer.nvim"
 -- Configure packer
 local packer = require "packer"
 packer.init {
-	display = {open_fn = function() return require"packer.util".float {border = "single"} end, prompt_border = "single"},
 	auto_clean = true,
 	compile_on_sync = true
 }
