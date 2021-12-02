ff683dad5e5e86365e0c5442750e03f112c2ea9c
ikerurda <ikerurda@gmail.com>
2021-12-02 20:58:49 +0100

    changed guicursor

--- lua/iker/options.lua
+++ lua/iker/options.lua
@@ -41,6 +41,7 @@ o.breakindent = true -- wrapped lines will maitain indentation
 o.linebreak = true -- wrap lines at a character in 'breakat' rather the las char
 
 o.clipboard = "unnamedplus"
+o.guicursor = "n-v-c-sm:block,i-ci-ve:hor25,r-cr-o:hor25"
 o.mouse = "a" -- Mouse in all modes
 o.belloff = "all" -- Just turn the dang bell off
 o.termguicolors = true -- Enables more colors
