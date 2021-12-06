859dad6a1010ca1aa3761da7f7f1108b38955d89
ikerurda <ikerurda@gmail.com>
2021-12-06 18:43:25 +0100

    changed init.lua heading

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
+--                          o8o
+-- ooo. .oo.  oooo    ooo oooo  ooo. .oo.  .oo.
+-- `888P"Y88b  `88.  .8'  `888  `888P"Y88bP"Y88b
+--  888   888   `88..8'    888   888   888   888
+--  888   888    `888'     888   888   888   888
+-- o888o o888o    `8'     o888o o888o o888o o888o
 --
---  ░▀█▀░▒█▄░▒█░▀█▀░▀▀█▀▀░░░░▒█░░▒█░▀█▀░▒█▀▄▀█
---  ░▒█░░▒█▒█▒█░▒█░░░▒█░░░▄▄░░▒█▒█░░▒█░░▒█▒█▒█
---  ░▄█▄░▒█░░▀█░▄█▄░░▒█░░░▀▀░░░▀▄▀░░▄█▄░▒█░░▒█
---
---  Config file for NVIM. Created by Iker Urdaniz.
+--  Config files for NEOVIM. Created by Iker Urdaniz.
 --
 pcall(require, "impatient")
 
