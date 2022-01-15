dde8005f35974144e203930b3f4c2639ad190af1
Iker Urdaniz <ikerurda@gmail.com>
2022-01-15 18:42:25 +0100

    added more mappings

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -16,6 +16,18 @@ local open_in = function(finder)
   end
 end
 
+local open_in_fb = function(prompt_bufnr)
+  local action_state = require "telescope.actions.state"
+  local Path = require "plenary.path"
+  local actions = require "telescope.actions"
+  local fb = require("telescope").extensions.file_browser.file_browser
+
+  local entry = action_state.get_selected_entry()[1]
+  local entry_path = Path:new(entry):parent():absolute()
+  actions._close(prompt_bufnr, true)
+  fb { path = entry_path }
+end
+
 local layout_actions = require "telescope.actions.layout"
 tl.setup {
   defaults = {
@@ -36,9 +48,22 @@ tl.setup {
     },
     mappings = {
       i = { ["<c-space>"] = layout_actions.toggle_preview },
+      n = { ["<space>"] = layout_actions.toggle_preview },
     },
   },
   pickers = {
+    oldfiles = {
+      mappings = {
+        i = { ["<a-e>"] = open_in_fb },
+        n = { ["<a-e>"] = open_in_fb },
+      },
+    },
+    find_files = {
+      mappings = {
+        i = { ["<a-e>"] = open_in_fb },
+        n = { ["<a-e>"] = open_in_fb },
+      },
+    },
     buffers = {
       ignore_current_buffer = true,
       sort_mru = true,
@@ -58,11 +83,16 @@ tl.setup {
     file_browser = {
       dir_icon = "#",
       path_display = { ["truncate"] = 3 },
+      previewer = false,
       cwd_to_path = true,
       grouped = true,
       respect_gitignore = false,
       mappings = {
         i = {
+          ["<a-f>"] = open_in(builtin.find_files),
+          ["<a-g>"] = open_in(builtin.live_grep),
+        },
+        n = {
           ["<a-f>"] = open_in(builtin.find_files),
           ["<a-g>"] = open_in(builtin.live_grep),
         },
