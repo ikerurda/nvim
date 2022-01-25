0a6cbdaeb377c066ba4574da10b864c9d3c6bed9
Iker Urdaniz <ikerurda@gmail.com>
2022-01-26 00:39:17 +0100

    added dir previewer

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -3,39 +3,68 @@ if not has_tl then
   return
 end
 
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+local layout_actions = require "telescope.actions.layout"
+local Path = require "plenary.path"
+
 local builtin = require "telescope.builtin"
 local open_in = function(finder)
   return function(prompt_bufnr)
-    local actions = require "telescope.actions"
-    local action_state = require "telescope.actions.state"
-    local entry_path = action_state.get_selected_entry().Path
-    local path = entry_path:is_dir() and entry_path:absolute()
-      or entry_path:parent():absolute()
+    local current_picker = action_state.get_current_picker(prompt_bufnr)
+    local path = current_picker.finder.path
     actions._close(prompt_bufnr, true)
     finder { cwd = path }
   end
 end
 
 local open_in_fb = function(prompt_bufnr)
-  local action_state = require "telescope.actions.state"
-  local Path = require "plenary.path"
-  local actions = require "telescope.actions"
   local fb = require("telescope").extensions.file_browser.file_browser
-
   local entry = action_state.get_selected_entry()[1]
   local entry_path = Path:new(entry):parent():absolute()
   actions._close(prompt_bufnr, true)
   fb { path = entry_path }
 end
 
-local layout_actions = require "telescope.actions.layout"
+local bookmarks = {
+  ["config"] = "~/.config/nvim",
+  ["web"] = "~/Sites",
+  ["plugins"] = "~/.local/share/nvim/site/pack/packer/start/",
+}
+
+local open_bookmark = function(prompt_bufnr)
+  local fb_utils = require "telescope._extensions.file_browser.utils"
+  local current_picker = action_state.get_current_picker(prompt_bufnr)
+  local finder = current_picker.finder
+
+  if not bookmarks or vim.tbl_isempty(bookmarks) then
+    print "[telescope] Please set some bookmarks first"
+    return
+  end
+
+  vim.ui.select(
+    vim.tbl_keys(bookmarks),
+    { prompt = "Select bookmark:" },
+    function(selection)
+      if selection then
+        finder.path = vim.fn.expand(bookmarks[selection])
+        fb_utils.redraw_border_title(current_picker)
+        current_picker:refresh(
+          finder,
+          { reset_prompt = true, multi = current_picker._multi }
+        )
+      end
+    end
+  )
+end
+
 tl.setup {
   defaults = {
     winblend = 10,
     prompt_prefix = " ",
     selection_caret = "  ",
     multi_icon = "• ",
-    path_display = { "smart" },
+    path_display = { ["truncate"] = 3 },
     sorting_strategy = "ascending",
     layout_strategy = "vertical",
     layout_config = {
@@ -81,23 +110,24 @@ tl.setup {
     },
     file_browser = {
       dir_icon = "#",
-      path_display = { ["truncate"] = 3 },
       dir_preview = function(path)
-        local Path = require "plenary.path"
         path = Path:new(path):make_relative(vim.loop.cwd())
         return "tree -F --noreport " .. path .. " | less -Sq"
       end,
-      cwd_to_path = true,
       grouped = true,
+      cwd_to_path = true,
+      open_file = true,
       respect_gitignore = false,
       mappings = {
         i = {
           ["<a-f>"] = open_in(builtin.find_files),
           ["<a-g>"] = open_in(builtin.live_grep),
+          ["<c-b>"] = open_bookmark,
         },
         n = {
           ["<a-f>"] = open_in(builtin.find_files),
           ["<a-g>"] = open_in(builtin.live_grep),
+          ["b"] = open_bookmark,
         },
       },
     },
